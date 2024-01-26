062b884b0c8204d5a118807fa3875f46547920e2
Funsatek Films <70167418+funsatekfilms@users.noreply.github.com>
2024-01-26 19:41:27 -0400

    Add files via upload

---
+++ footer.js
@@ -0,0 +1,6 @@
+var copyrightText = document.getElementById('copyright-text');
+var rightsReservedText = document.getElementById('rights-reserved-text');
+
+copyrightText.innerHTML = '&copy; Copyright 2024 Funsatek Films.';
+
+rightsReservedText.innerHTML = 'All Rights Reserved';
